3e67f8a354b60023321fbade8d5683909b6719a1
aazizisoufiane <aazizi.soufiane@gmail.com>
2023-09-19 16:44:58 +0000

    add workflow preprocess

---
+++ config.py
@@ -0,0 +1,28 @@
+from omegaconf import DictConfig
+import yaml
+import os
+
+_config_name = 'orchestrator.yaml'
+
+def get_config(config_name: str):
+    """
+    Load a YAML configuration file and return it as a DictConfig.
+
+    Args:
+        config_name (str): The name of the configuration file.
+
+    Returns:
+        DictConfig: The loaded configuration as a DictConfig object.
+    """
+    # Construct the full path to the configuration file
+    config_file = f"config/{config_name}"
+
+    # Load and parse the YAML configuration file
+    with open(config_file, 'r') as file:
+        conf = yaml.safe_load(file)
+    
+    return DictConfig(conf)
+
+# Load the configuration file
+config = get_config(_config_name)
+
